d873a09507bd2bc01e29dbcfbcccb3c84f3e386b
Michael Nelson <michael@eventespresso.com>
2015-01-08 11:21:34 -0800

    [touch:5836]{t:20}added repo for flexible payment method

---
+++ espresso-flexible-payment-method.php
@@ -0,0 +1,54 @@
+<?php
+/*
+  Plugin Name: Event Espresso - Flexible Payment Method
+  Plugin URI: http://www.eventespresso.com
+  Description: The Event Espresso flexible payment method adds a simple textarea for payment information (e.g. offline or online payment instructions).
+
+  Version: 1.0
+
+  Author: Event Espresso
+  Author URI: http://www.eventespresso.com
+  Copyright 2014 Event Espresso (email : support@eventespresso.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA02110-1301USA
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Event Espresso
+ *
+ * Event Registration and Management Plugin for WordPress
+ *
+ * @ package		Event Espresso
+ * @ author			Event Espresso
+ * @ copyright	(c) 2008-2014 Event Espresso  All Rights Reserved.
+ * @ license		http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
+ * @ link				http://www.eventespresso.com
+ * @ version	 	EE4
+ *
+ * ------------------------------------------------------------------------
+ */
+define( 'EE_FLEXIBLE_VERSION', '1.0' );
+define( 'EE_FLEXIBLE_PLUGIN_FILE',  __FILE__ );
+
+function load_espresso_flexible() {
+  if ( class_exists( 'EE_Addon' ) ) {
+  	require_once ( plugin_dir_path( __FILE__ ) . 'EE_Flexible_Payment_Method.class.php' );
+  	EE_Flexible_Payment_Method::register_addon();
+  }
+}
+add_action( 'AHEE__EE_System__load_espresso_addons', 'load_espresso_flexible' );
+
+
+// End of file espresso_flexible_payment_method.php
+// Location: wp-content/plugins/ee4-flexible-payment-method/espresso-flexible-payment-method.php
---
+++ payment_methods/Flexible_Onsite/help_tabs/payment_methods_overview_flexible.help_tab.php
@@ -0,0 +1,19 @@
+<h3><?php _e('Flexible', 'event_espresso'); ?></h3>
+<p>
+<?php _e('Adjust the settings for the flexible payment method. This is a flexible payment option that allows you to provide your attendees/registrants with instructions on how to complete their payment (e.g. pay at the door, call a number to complete a payment, etc).', 'event_espresso'); ?>
+</p>
+<h3><?php _e('Flexible Settings', 'event_espresso'); ?></h3>
+<ul>
+<li>
+<strong><?php _e('Title', 'event_espresso'); ?></strong><br />
+<?php _e('Enter a title or name for this payment method.', 'event_espresso'); ?>
+</li>
+<li>
+<strong><?php _e('Payment Instructions', 'event_espresso'); ?></strong><br />
+<?php _e('Provide clear instructions for how a payment should be made.', 'event_espresso'); ?>
+</li>
+<li>
+<strong><?php _e('Button Image URL', 'event_espresso'); ?></strong><br />
+<?php _e('Change the image that is used for this payment method.', 'event_espresso'); ?>
+</li>
+</ul>
