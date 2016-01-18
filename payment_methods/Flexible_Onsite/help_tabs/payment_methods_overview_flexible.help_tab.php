87f7156c250ddb7a1652389d01deb936ebff0fe2
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2016-01-18 18:03:21 -0500

    Add paragraph tags for a11y. [touch:9256]

--- payment_methods/Flexible_Onsite/help_tabs/payment_methods_overview_flexible.help_tab.php
+++ payment_methods/Flexible_Onsite/help_tabs/payment_methods_overview_flexible.help_tab.php
@@ -1,8 +1,8 @@
-<strong><?php _e('Flexible', 'event_espresso'); ?></strong>
+<p><strong><?php _e('Flexible', 'event_espresso'); ?></strong></p>
 <p>
 <?php _e('Adjust the settings for the flexible payment method. This is a flexible payment option that allows you to provide your attendees/registrants with instructions on how to complete their payment (e.g. pay at the door, call a number to complete a payment, etc).', 'event_espresso'); ?>
 </p>
-<strong><?php _e('Flexible Settings', 'event_espresso'); ?></strong>
+<p><strong><?php _e('Flexible Settings', 'event_espresso'); ?></strong></p>
 <ul>
 <li>
 <strong><?php _e('Title', 'event_espresso'); ?></strong><br />
