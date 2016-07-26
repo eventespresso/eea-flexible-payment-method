d8e0f849313419201be971df434d84564cd1876b
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2016-07-26 12:06:48 -0400

    Add intro text for flexible payment. [touch:9921]

---
+++ payment_methods/Flexible_Onsite/templates/flexible_onsite_intro.template.php
@@ -0,0 +1,1 @@
+<?php echo esc_html__('Flexible payments is an off-site payment option for accepting payments. You can provide your registrants/attendee with specific information on how to pay for their registrations (e.g. call a phone number to pay, pay at the event).', 'event_espresso');
