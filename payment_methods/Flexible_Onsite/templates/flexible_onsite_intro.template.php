5806888869b84d9eac1c4f04fc301e0a7e5c0308
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2016-07-28 18:21:43 -0400

    esc_html_e the flexible payment method. [touch:9921]

--- payment_methods/Flexible_Onsite/templates/flexible_onsite_intro.template.php
+++ payment_methods/Flexible_Onsite/templates/flexible_onsite_intro.template.php
@@ -1,1 +1,1 @@
-<?php echo esc_html__('Flexible payments is an off-site payment option for accepting payments. You can provide your registrants/attendee with specific information on how to pay for their registrations (e.g. call a phone number to pay, pay at the event, etc).', 'event_espresso');
+<?php esc_html_e('Flexible payments is an off-site payment option for accepting payments. You can provide your registrants/attendee with specific information on how to pay for their registrations (e.g. call a phone number to pay, pay at the event, etc).', 'event_espresso');
