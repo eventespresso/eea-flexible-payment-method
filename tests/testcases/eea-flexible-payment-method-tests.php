fc9a68ef638251b11e3d68055ca416a3891125d6
Darren Ethier <darren@roughsmootheng.in>
2017-04-20 15:29:00 -0400

    update bootstrap and config for tests on eea-flexible-payment-method [touch: 10623]

---
+++ tests/testcases/eea-flexible-payment-method-tests.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Test class for eea-flexible-payment method main file
+ *
+ * @since         1.0.0
+ * @package       EventEspresso\FlexiblePaymentMethod
+ * @subpackage    tests
+ */
+class eea_flexible_payment_method_tests extends EE_UnitTestCase
+{
+
+    /**
+     * Tests the loading of the main file
+     *
+     * @since 0.0.1.dev.002
+     */
+    function test_loading_new_payment_method()
+    {
+        $this->assertEquals(has_action('AHEE__EE_System__load_espresso_addons', 'load_espresso_new_payment_method'),
+            10);
+        $this->assertTrue(class_exists('EE_Flexible_Payment_Method'));
+    }
+}
